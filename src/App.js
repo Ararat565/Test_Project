96a85af05f21552e58e429fccf5d3612e243b092
install install <install@gtc.eif.local>
2023-05-10 20:49:37 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import { useEffect, useState } from "react";
+
+
+function App() {
+
+  const[git, setGit] = useState([]);
+
+
+  useEffect(()=>{
+    fetch ("https://api.github.com/users")
+    .then((p1)=> p1.json())
+    .then((p2)=>{
+      setGit(p2)
+      console.log(p2)
+    })
+  },[])
+
+
+  return (
+    <div> 
+      {git.map((item, index)=>
+        <li key={index}>
+        {item.id}  
+        <img src={item.avatar_url} height="50" width="50"/>
+        </li>)}
+    </div>
+  );
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+export default App;
